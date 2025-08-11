d1adf785cb22ce1b745673a82a0bcdaa048f2c8c
Zimer-Gbz <gbz5@hotmail.com>
2025-08-11 09:51:59 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
-
-# lê o conteúdo do README.md
 from os import path
+
+# Lê o conteúdo do README.md para usar como descrição longa
 this_directory = path.abspath(path.dirname(__file__))
 with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
@@ -11,15 +11,17 @@ setup(
     version='0.1',
     description='Plugin de controle avançado de fermentação para CraftBeerPi 4 inspirado no BrewPiLess',
     author='Gustavo Zimer',
+    author_email='',  # opcional
     url='https://github.com/Zimer-Gbz/cbpi4-BrewPiLessAdvancedFermentationController',
     include_package_data=True,
     package_data={
-        # inclui arquivos adicionais como .yaml, .json, etc
+        # Inclui arquivos extras como config.yaml
         '': ['*.txt', '*.rst', '*.yaml', '*.json'],
-        'cbpi4-BrewPiLessAdvancedFermentationController': ['*', '*.txt', '*.rst', '*.yaml', '*.json']
+        'cbpi4-BrewPiLessAdvancedFermentationController': [
+            '*', '*.txt', '*.rst', '*.yaml', '*.json'
+        ]
     },
     packages=['cbpi4-BrewPiLessAdvancedFermentationController'],
     long_description=long_description,
     long_description_content_type='text/markdown'
 )
-
